1d640d5b355b76389ff6d8ad17bb47450c4b4cfb
Samuel <fernandesamuel2002@gmail.com>
2025-11-23 17:23:27 +0530

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
-import authService from './appwrite/auth';
-import { login, logout } from './store/authSlice';
-import { Header, Footer } from './components';
-import AddPost from './pages/AddPost';
-import AllPosts from './pages/AllPosts';
-import EditPost from './pages/EditPost';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Post from './pages/Post';
-import Signup from './pages/Signup';
-import './App.css';
-
-// Protect private routes
-function ProtectedRoute() {
-  const auth = useSelector(state => state.auth);
-  return auth.user ? <Outlet /> : <Navigate to="/login" />;
-}
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+import './App.css'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth);
 
+  const [loading, setLoading] = useState(true);  //loading state to track auth status, this is alos done for, when we run the application then useEffect hook calls getCurrentUser to check if user is logged in or not, during this time we don't want to render the application until we get the response from getCurrentUser method.(Here loading true means we are still checking the auth status and loading false means we have got the response)
+  const dispatch = useDispatch();
+
+  // App.jsx (Your file, minor update for clarity)
   useEffect(() => {
     authService.getCurrentUser()
-      .then(userData => {
+      .then((userData) => {
         if (userData) {
           dispatch(login({ userData }));
         } else {
@@ -34,42 +22,26 @@ function App() {
         }
       })
       .catch(() => {
+        // User is not logged in (still a guest), so mark as logged out in Redux
         dispatch(logout());
       })
-      .finally(() => setLoading(false));
-  }, [dispatch]);
+      .finally(() => setLoading(false)); //finally executes at the end of promise whether it is resolved or rejected
+  }, []);
 
-  if (loading) return null; // wait for authentication check
 
-  return (
+
+  return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
-      {/* Always show header.
-          Pass logged-in state so header can conditionally render Auth buttons. */}
-      <Header isAuthenticated={!!auth.user} />
-
-      <main>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-
-          {/* Protected routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/add-post" element={<AddPost />} />
-            <Route path="/all-posts" element={<AllPosts />} />
-            <Route path="/edit-post/:id" element={<EditPost />} />
-            <Route path="/post/:id" element={<Post />} />
-          </Route>
-
-          {/* Fallback */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-      {/* Always show footer */}
-      <Footer />
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-  );
+  ) : null
+
 }
 
-export default App;
+export default App
